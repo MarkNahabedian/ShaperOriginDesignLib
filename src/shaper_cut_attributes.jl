95c0257c766aff8e5965b57677931b25df3ac691
MarkNahabedian <naha@mit.edu>
2021-09-13 19:47:12 -0400

    Try to express Shaper styles in a way both Julia and SVG can cope with.

--- src/shaper_cut_attributes.jl
+++ src/shaper_cut_attributes.jl
@@ -3,38 +3,38 @@
 export shaper_cut_attributes
 
 shaper_cut_attributes = Dict(
-	:inside_cut => (
-		fill = "white",
-		stroke = "black",
-		stroke_width = "0.01",
-		opacity="1.0"),
+	:inside_cut => Dict(
+		"fill" => "white",
+		"stroke" => "black",
+		"stroke-width" => "0.01",
+		"opacity" => "1.0"),
 	:outside_cut => (
-		fill=  "black",
-		stroke = "black",
-		stroke_width = "0.01",
-		opacity = "1.0"
+		"fill" => "black",
+		"stroke" => "black",
+		"stroke-width" => "0.01",
+		"opacity" => "1.0"
 	),
 	:on_line_cut => (
-		fill=  "none",
-		stroke = "gray",
-		stroke_width = "0.01",
-		opacity = "1.0"
+		"fill" => "none",
+		"stroke" => "gray",
+		"stroke-width" => "0.01",
+		"opacity" => "1.0"
 	),
 	:on_line_cut => (
-		fill=  "none",
-		stroke = "gray",
-		stroke_width = "0.01",
-		opacity = "1.0"
+		"fill" => "none",
+		"stroke" => "gray",
+		"stroke-width" => "0.01",
+		"opacity" => "1.0"
 	),
 	:pocket_cut => (
-		fill=  "gray",
-		stroke = "none",
-		opacity = "1.0"
+		"fill" => "gray",
+		"stroke" => "none",
+		"opacity" => "1.0"
 	),
 	:guide_line => (
-		fill=  "none",
-		stroke = "blue",
-		stroke_width = "0.01",
-		opacity = "1.0"
+		"fill" => "none",
+		"stroke" => "blue",
+		"stroke-width" => "0.01",
+		"opacity" => "1.0"
 	)
 )
