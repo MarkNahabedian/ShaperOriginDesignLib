75fec7c930d7766266fb295f77e0b8f89286ef06
MarkNahabedian <naha@mit.edu>
2021-09-13 20:10:46 -0400

    Make the previous change at least be consistent.

--- src/shaper_cut_attributes.jl
+++ src/shaper_cut_attributes.jl
@@ -8,30 +8,30 @@ shaper_cut_attributes = Dict(
 		"stroke" => "black",
 		"stroke-width" => "0.01",
 		"opacity" => "1.0"),
-	:outside_cut => (
+	:outside_cut => Dict(
 		"fill" => "black",
 		"stroke" => "black",
 		"stroke-width" => "0.01",
 		"opacity" => "1.0"
 	),
-	:on_line_cut => (
+	:on_line_cut => Dict(
 		"fill" => "none",
 		"stroke" => "gray",
 		"stroke-width" => "0.01",
 		"opacity" => "1.0"
 	),
-	:on_line_cut => (
+	:on_line_cut => Dict(
 		"fill" => "none",
 		"stroke" => "gray",
 		"stroke-width" => "0.01",
 		"opacity" => "1.0"
 	),
-	:pocket_cut => (
+	:pocket_cut => Dict(
 		"fill" => "gray",
 		"stroke" => "none",
 		"opacity" => "1.0"
 	),
-	:guide_line => (
+	:guide_line => Dict(
 		"fill" => "none",
 		"stroke" => "blue",
 		"stroke-width" => "0.01",
