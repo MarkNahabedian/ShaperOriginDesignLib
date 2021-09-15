30f8baa9a2d8d2f04a8d9ff0c1b137b2332c2ac8
MarkNahabedian <naha@mit.edu>
2021-09-15 16:58:15 -0400

    Depends on NativeSVG.  Reindent custom_anchor.

--- src/custom_anchor.jl
+++ src/custom_anchor.jl
@@ -1,16 +1,18 @@
 
+using NativeSVG
+
 export custom_anchor
 
 ```
-Mark the location of a custom anchor for Shaper Origin.
-```
+    Mark the location of a custom anchor for Shaper Origin.
+    ```
 function custom_anchor(io, x, y; xdirection=1, ydirection=-1, size=0.25u"inch")
-	path(io; style="fill:red;
-		stroke=none;",
-		d=pathd(
-			["M", x, y],
-			["h", xdirection * size],
-			["L", x, y + ydirection * 2 * size],
-			["z"]
-			))
+    path(io; style="fill:red;
+		    stroke=none;",
+	 d=pathd(
+	     ["M", x, y],
+	     ["h", xdirection * size],
+	     ["L", x, y + ydirection * 2 * size],
+	     ["z"]
+	 ))
 end
