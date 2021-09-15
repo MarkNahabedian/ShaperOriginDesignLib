2926caa03c6f1d097f9338368f0f81ba1ad235d6
MarkNahabedian <naha@mit.edu>
2021-09-15 17:10:00 -0400

    Remove spurious newline in style string.

--- src/custom_anchor.jl
+++ src/custom_anchor.jl
@@ -7,8 +7,8 @@ export custom_anchor
     Mark the location of a custom anchor for Shaper Origin.
     ```
 function custom_anchor(io, x, y; xdirection=1, ydirection=-1, size=0.25u"inch")
-    path(io; style="fill:red;
-		    stroke=none;",
+    path(io;
+         style="fill:red; stroke=none;",
 	 d=pathd(
 	     ["M", x, y],
 	     ["h", xdirection * size],
