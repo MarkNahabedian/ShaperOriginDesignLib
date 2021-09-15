027f7f7bf6a36fb3d382e05cff5db1cbddea895b
MarkNahabedian <naha@mit.edu>
2021-09-15 15:17:27 -0400

    Custom anchors for Shaper Origin.

--- src/ShaperOriginDesignLib.jl
+++ src/ShaperOriginDesignLib.jl
@@ -10,5 +10,6 @@ SVG_NAMESPACE = "http://www.w3.org/2000/svg"
 
 include("shaper_cut_attributes.jl")
 include("svg_utils.jl")
+include("custom_anchor.jl")
 
 end
---
+++ src/custom_anchor.jl
@@ -0,0 +1,16 @@
+
+export custom_anchor
+
+```
+Mark the location of a custom anchor for Shaper Origin.
+```
+function custom_anchor(io, x, y; xdirection=1, ydirection=-1, size=0.25u"inch")
+	path(io; style="fill:red;
+		stroke=none;",
+		d=pathd(
+			["M", x, y],
+			["h", xdirection * size],
+			["L", x, y + ydirection * 2 * size],
+			["z"]
+			))
+end
