6d8e5a49943a05c639a1f9e858319686e9d77813
MarkNahabedian <naha@mit.edu>
2023-08-02 18:40:55 -0400

    Use regular string instead of markdown string for function doc.

--- src/custom_anchor.jl
+++ src/custom_anchor.jl
@@ -2,12 +2,12 @@
 
 export custom_anchor
 
-```
+"""
 custom_anchor(x, y; xdirection=1, ydirection=-1, size=0.25u"inch")
 
 Mark the location of a custom anchor for Shaper Origin.
 Returns an SVG `path` element.
-```
+"""
 function custom_anchor(x, y; xdirection=1, ydirection=-1, size=0.25u"inch")
     elt("path",
         style => "fill:red; stroke=none;",
