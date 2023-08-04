b642113f01739a6a2c0e618829ed153b9bfa0a7b
MarkNahabedian <naha@mit.edu>
2023-08-04 10:19:30 -0400

    To avoid version conflicts for XML, copy the definition of elt, rather than importing it from TabletWeaving.

--- src/ShaperOriginDesignLib.jl
+++ src/ShaperOriginDesignLib.jl
@@ -4,13 +4,13 @@ using Printf
 using Unitful
 using UnitfulUS
 using XML
-using TabletWeaving: elt
 
 export SVG_NAMESPACE, SHAPER_NAMESPACE
 
 SVG_NAMESPACE = "http://www.w3.org/2000/svg"
 SHAPER_NAMESPACE = "http://www.shapertools.com/namespaces/shaper"
 
+include("elt.jl")
 include("shaper_cut_attributes.jl")
 include("svg_utils.jl")
 include("custom_anchor.jl")
