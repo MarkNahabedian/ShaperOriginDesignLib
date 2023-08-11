3c217cf28e46a1ea5637d6071f1f74bda4bba5a2
MarkNahabedian <naha@mit.edu>
2023-08-11 15:22:09 -0400

    Code cleanup.  NightstandModel.

--- examples/PinesCornerNightstand/script.jl
+++ examples/PinesCornerNightstand/script.jl
@@ -21,6 +21,8 @@ SVG_MARGIN = 3u"inch"
 
 # The bed is 27" high, so the table should be about that height
 HEIGHT = 27u"inch"
+TOP_THICKNESS = 0.5u"inch"
+
 
 # Maximum hypotenuse length is 32"
 # Why didn't I measure the distance of the bed from the wall?
@@ -28,7 +30,10 @@ TRIANGLE_LEG_DISTANCE = 20u"inch"
 
 LEG_THICKNESS = 1u"inch"
 LEG_INSET = 1u"inch"
+TENON_LENGTH = 0.75u"inch"
 
+
+################################################################################
 
 struct Leg
     x1
@@ -48,6 +53,26 @@ height(leg::Leg) = leg.y2 - leg.y1
 center(leg::Leg) = ((leg.x1 + leg.x2) / 2,
                     (leg.y1 + leg.y2) / 2)
      
+
+function svg(leg::Leg, attrs...)
+    elt("g",
+        :class => "Leg",
+        elt("rect",
+            :x => svgval(leg.x1),
+            :y => svgval(leg.y1),
+            :width => svgval(width(leg)),
+            :height => svgval(height(leg)),
+            attrs...,)
+        #=
+        elt("path", :d => pathd([ "M", leg.x2, leg.y1 ],
+                                [ "L", leg.x1, leg.y2 ])),
+        elt("path", :d => pathd([ "M", leg.x1, leg.y1 ],
+                                [ "L", leg.x2, leg.y2 ]))
+        =#
+        )
+end
+
+################################################################################
 
 function arc_point(x, y, direction, radius)
     d = rem(direction, 360°)
@@ -75,56 +100,83 @@ let
 end
 
 
-function svg(leg::Leg, attrs...)
-    elt("g",
-        :class => "Leg",
-        attrs...,
-        elt("rect",
-            :x => svgval(leg.x1),
-            :y => svgval(leg.y1),
-            :width => svgval(width(leg)),
-            :height => svgval(height(leg)))
-        #=
-        elt("path", :d => pathd([ "M", leg.x2, leg.y1 ],
-                                [ "L", leg.x1, leg.y2 ])),
-        elt("path", :d => pathd([ "M", leg.x1, leg.y1 ],
-                                [ "L", leg.x2, leg.y2 ]))
-        =#
-        )
+################################################################################
+
+
+struct NightstandModel
+    nightstand_height
+    top_thickness
+    triangle_leg_distance
+    leg_inset
+    leg_thickness
+    tenon_length
+
+    # Derived properties:
+    imaginary_right_angle_leg
+    raleg1
+    raleg2
+    leg1
+    leg2
+
+    function NightstandModel(;
+                             nightstand_height,
+                             top_thickness,
+                             triangle_leg_distance,
+                             leg_inset,
+                             leg_thickness,
+                             tenon_length)
+        new(nightstand_height,
+            top_thickness,
+            triangle_leg_distance,
+            leg_inset,
+            leg_thickness,
+            tenon_length,
+            Leg(LEG_INSET, LEG_INSET, LEG_THICKNESS),  # imaginary_right_angle_leg
+            Leg(LEG_INSET, LEG_INSET + LEG_THICKNESS, LEG_THICKNESS),  # raleg1
+            Leg(LEG_INSET + LEG_THICKNESS, LEG_INSET, LEG_THICKNESS),  # raleg2
+            Leg(LEG_INSET, TRIANGLE_LEG_DISTANCE - LEG_INSET, LEG_THICKNESS), # leg1
+            Leg(TRIANGLE_LEG_DISTANCE - LEG_INSET, LEG_INSET, LEG_THICKNESS)  # leg2
+            )
+    end
 end
 
 
-function top_outline()
-    corner_radius = LEG_INSET + LEG_THICKNESS / 2
-    imaginary_right_angle_leg = Leg(LEG_INSET, LEG_INSET, LEG_THICKNESS)
-    # At the right angle corner we will really have two legs joined by
-    # hinges:
-    raleg1 = Leg(LEG_INSET, LEG_INSET + LEG_THICKNESS, LEG_THICKNESS)
-    raleg2 = Leg(LEG_INSET + LEG_THICKNESS, LEG_INSET, LEG_THICKNESS)
-    leg1 = Leg(LEG_INSET, TRIANGLE_LEG_DISTANCE - LEG_INSET, LEG_THICKNESS)
-    leg2 = Leg(TRIANGLE_LEG_DISTANCE - LEG_INSET, LEG_INSET, LEG_THICKNESS)
+leg_length(nsm::NightstandModel) =
+    nsm.nightstand_height - nsm.top_thickness
+
+leg_count(nsm::NightstandModel) = 4
+
+stringer_count(nsm::NightstandModel) = 4
+
+stringer_length(nsm::NightstandModel) =
+    nsm.triangle_leg_distance + 2 * nsm.tenon_length
+
+
+################################################################################
+
+
+function top_outline(nsm::NightstandModel)
+    corner_radius = nsm.leg_inset
+    imaginary_right_angle_leg = nsm.imaginary_right_angle_leg
+    raleg1 = nsm.raleg1 
+    raleg2 = nsm.raleg2
+    leg1 = nsm.leg1
+    leg2 = nsm.leg2
     leg_rect_args = [
-        :style => "fill: none; stroke: green; stroke-width: 0.01;"
+        :style => shaper_style_string(:guide_line)
     ]
     elt("svg",
         namespace_attributes()...,
         viewport_attributes(
             - SVG_MARGIN,
             - SVG_MARGIN,
-            TRIANGLE_LEG_DISTANCE + SVG_MARGIN,
-            TRIANGLE_LEG_DISTANCE + SVG_MARGIN,
-            u"inch" #, false
-	)...,
+            nsm.triangle_leg_distance + SVG_MARGIN,
+            nsm.triangle_leg_distance + SVG_MARGIN,
+            u"inch", false)...,
 	:width => "90%",
-	:style => "background-color: pink",
         elt("g",
             # Invert Y axis for conventional coordinate system:
-            :transform => "scale(1 -1) translate(0, $(TRIANGLE_LEG_DISTANCE))",
-            svg(raleg1, leg_rect_args...),
-            svg(raleg2, leg_rect_args...),
-            svg(leg1, leg_rect_args...),
-            svg(leg2, leg_rect_args...),
-            #=
+            :transform => "translate(0, $(svgval(nsm.triangle_leg_distance))) scale(1 -1)",
             elt("path",
                 :style => shaper_style_string(:guide_line),
                  :d => pathd(
@@ -132,9 +184,8 @@ function top_outline()
                     [ "L", center(imaginary_right_angle_leg)... ],
                     [ "L", center(leg2)... ],
                     [ "L", center(leg1)... ])),
-            =#
             elt("path",
-                :style => shaper_style_string(:guide_line),
+                :style => shaper_style_string(:pocket_cut),
                 :d => pathd(
                     [ "M", arc_point(center(leg1)..., 180°, corner_radius)... ],
                     [ "L", arc_point(center(imaginary_right_angle_leg)...,
@@ -143,17 +194,18 @@ function top_outline()
                       0, 0, 1,
                       arc_point(center(imaginary_right_angle_leg)...,
                                      -90°, corner_radius)... ],
-                    [ "M", arc_point(center(imaginary_right_angle_leg)...,
-                                     -90°, corner_radius)... ],
                     [ "L", arc_point(center(leg2)..., -90°, corner_radius)... ],
                     [ "A", corner_radius, corner_radius,
                       0, 0, 1,
                       arc_point(center(leg2)..., 45°, corner_radius)... ],
-                    [ "M", arc_point(center(leg2)..., 45°, corner_radius)... ],
                     [ "L", arc_point(center(leg1)..., 45°, corner_radius)... ],
                     [ "A", corner_radius, corner_radius,
                       0, 0, 1,
                       arc_point(center(leg1)..., 180°, corner_radius)... ])),
+            svg(raleg1, leg_rect_args...),
+            svg(raleg2, leg_rect_args...),
+            svg(leg1, leg_rect_args...),
+            svg(leg2, leg_rect_args...),
             center_mark(center(raleg1)...),
             center_mark(center(raleg2)...),
             center_mark(center(leg1)...),
@@ -161,36 +213,46 @@ function top_outline()
             ))
 end
 
-
-let
-    svg = top_outline()
-    html = elt("html",
-		elt("head"),
-		elt("body",
-			svg
-		)
-	)
-    XML.write("top.svg", svg)
-    HTML(XML.write(html))
+function write_top_outline_file(nsm::NightstandModel, filename)
+    svg = top_outline(nsm)
+    XML.write(filename, svg)
 end
 
 
-let
-    leg_count = 4
-    leg_length = HEIGHT   # does not yet subtract thickness of table top.
-    stringer_count = 4
-    stringer_length = TRIANGLE_LEG_DISTANCE  # a bit extra to provide
-                                             # for half way through
-                                             # tennons.
-    open("stock.txt", "w") do io
-        println(io, "$leg_count legs, $leg_length each.")
-        println(io, "$stringer_count stringers, $stringer_length each.")
+function write_measurement_file(nsm::NightstandModel, filename)
+    open(filename, "w") do io
+        println(io, "$(leg_count(nsm)) legs, $(leg_length(nsm)) each.")
+        println(io, "$(stringer_count(nsm)) stringers, $(stringer_length(nsm)) each.")
     end
 end
 
 
+################################################################################
+
+NIGHTSTAND_MODEL =
+    NightstandModel(;
+                    # The bed is 27" high, so the table should be about that height
+                    nightstand_height = 27u"inch",
+                    top_thickness = 0.5u"inch",
+                    # Maximum hypotenuse length is 32"
+                    # Why didn't I measure the distance of the bed from the wall?
+
+                    triangle_leg_distance = 20u"inch",
+                    leg_inset = 1u"inch",
+                    leg_thickness = 1u"inch",
+                    tenon_length = 0.75u"inch"
+                    )
+
+write_measurement_file(NIGHTSTAND_MODEL, "stock.txt")
+
+write_top_outline_file(NIGHTSTAND_MODEL, "top.svg")
+
+
+################################################################################
+
+
 # Dimensions of the hinge
-HINGE_THICKNESS = 0.03u"inch"       # mottice depth
+HINGE_THICKNESS = 0.03u"inch"       # mottice depths
 HINGE_LENGTH = 0.8u"inch"
 HINGE_WIDTH = 1.0u"inch"    # from edge of one leaf to the edge of the other
 
