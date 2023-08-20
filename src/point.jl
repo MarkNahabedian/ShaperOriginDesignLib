e84c6b848fb680f56317fe9c91704dafcf6f7a23
MarkNahabedian <naha@mit.edu>
2023-08-20 17:34:05 -0400

    {normalize.

--- src/point.jl
+++ src/point.jl
@@ -1,5 +1,5 @@
 
-export Point, dot, elementwise_product, distance, direction
+export Point, dot, elementwise_product, distance, normalize, direction
 
 struct Point
     x
@@ -24,6 +24,7 @@ end
 (Base.:-)(p1::Point, p2::Point) = Point(p1.x - p2.x, p1.y - p2.y)
 
 (Base.:*)(n::Number, p::Point) = Point(n * p.x, n * p.y)
+(Base.:/)(p::Point, n::Number) = Point(p.x / n, p.y / n)
 
 dot(p1::Point, p2::Point) = p1.x * p2.x + p1.y * p2.y
 
@@ -32,6 +33,8 @@ elementwise_product(p1::Point, p2::Point) = p1.*p2
 distance(x1, y1, x2, y2) = sqrt((x2 - x1)^2 + (y2 - y1)^2)
 
 distance(p::Point) = sqrt((p.x)^2 + (p.y)^2)
+
+normalize(p::Point) = p / distance(p)
 
 
 function direction(point1::Point, point2::Point)
