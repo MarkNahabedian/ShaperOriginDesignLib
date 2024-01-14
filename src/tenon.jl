29388525950eb9f32bfba608a0324cd7fd7fa62c
MarkNahabedian <naha@mit.edu>
2024-01-14 10:27:52 -0500

    SVG acts wonky if we specify units for rect and circle length attributes.

--- src/tenon.jl
+++ src/tenon.jl
@@ -8,7 +8,7 @@ export tenon
 ..._i and ..._j are the dimensions along each of two axes (looking at
 the stock end on) of the tenon itself and the end of the stock.
 
-Defines an outside cut for the rectangilar tenon and a pocket cut for
+Defines an outside cut for the rectangular tenon and a pocket cut for
 the rest of the stock.
 
 `cutter_diameter` is for the cutter that will be used for the mortise,
@@ -16,36 +16,38 @@ so the tenon can be rounded to match.
 
 The tenon is centered on the stock.
 
+The coordinate 0, 0 is at the top left corner of the stock.
+
 `extra` is how much bigger the pocket cut outline should be beyond the
-stock itself.
+stock itself.  It defaults to `cutter_diameter`.
 """
 function tenon(tenon_length, cutter_diameter,
-               tenon_i, tenon_j, stock_i, stock_j,
-               extra)
+               tenon_i, tenon_j, stock_i, stock_j;
+               extra=cutter_diameter)
     @assert cutter_diameter <=  tenon_i
     @assert cutter_diameter <= tenon_j
     elt("g",
         :class => "tenon",
         elt("rect",
-            :x => svglength(- extra),
-            :y => svglength(- extra),
-            :width => svglength(stock_i + 2 * extra),
-            :height => svglength(stock_j + 2 * extra),
+            :x => svgval(- extra),
+            :y => svgval(- extra),
+            :width => svgval(stock_i + 2 * extra),
+            :height => svgval(stock_j + 2 * extra),
             shaper_cut_depth(tenon_length),
             :style => shaper_style_string(:pocket_cut)),
         elt("rect",
-            :x => svglength(stock_i / 2 - tenon_i / 2),
-            :y => svglength(stock_j / 2 - tenon_j / 2),
-            :width => svglength(tenon_i),
-            :height => svglength(tenon_j),
-            :rx => svglength(cutter_diameter / 2),
-            :ry => svglength(cutter_diameter / 2),
+            :x => svgval(stock_i / 2 - tenon_i / 2),
+            :y => svgval(stock_j / 2 - tenon_j / 2),
+            :width => svgval(tenon_i),
+            :height => svgval(tenon_j),
+            :rx => svgval(cutter_diameter / 2),
+            :ry => svgval(cutter_diameter / 2),
             :style => shaper_style_string(:outside_cut)),
         elt("rect",
             :x => 0,
             :y => 0,
-            :width => svglength(stock_i),
-            :height => svglength(stock_j),
+            :width => svgval(stock_i),
+            :height => svgval(stock_j),
             :style => shaper_style_string(:guide_line)))
 end
 
